9768c391effc1e8ac4cde6f620c5a6e9d1200b1e
AbLlanos <abers2341@gmail.com>
2025-11-18 10:01:42 -0500

    V2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from "./components/general/nav-bar/nav-bar";
+import { Home } from "./components/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBar, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
